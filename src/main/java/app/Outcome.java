13b82706f72e56579485e47308899bc62b5e018d
S3973093 <S3973093@student.rmit.edu.au>
2022-10-15 19:58:14 +1300

    Create Outcome Class

---
+++ src/main/java/app/Outcome.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Outcome {
+
+    public String OutcomeID; 
+    public String Title;
+    public String Descrip;
+
+    public Outcome() {
+    }
+
+    public Outcome(String id, String title, String descrip) {
+        this.OutcomeID = id;
+        this.Title = title; 
+        this.Descrip = descrip;
+    }
+}

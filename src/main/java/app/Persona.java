31e4312e798ea8ef55820d5c5e8068a5edcad25f
Marielle Lopez <s3922406@student.rmit.edu.au>
2022-10-15 20:50:00 +1100

    Updated getter methods

--- src/main/java/app/Persona.java
+++ src/main/java/app/Persona.java
@@ -3,12 +3,16 @@ package app;
 public class Persona {
 
    private String name;
+   private int age;
+   private String ethnicity;
    private String quote;
    private String image_file_path;
    
    // Constructor
-   public Persona(String name, String quote, String image_file_path) {
+   public Persona(String name, int age, String ethnicity, String quote, String image_file_path) {
       this.name = name;
+      this.age = age;
+      this.ethnicity = ethnicity;
       this.quote = quote;
       this.image_file_path = image_file_path;
    }
@@ -16,6 +20,14 @@ public class Persona {
    // Getter methods
    public String getName() {
       return name;
+   }
+
+   public int getAge() {
+      return age;
+   }
+
+   public String getEthnicity() {
+      return ethnicity;
    }
 
    public String getQuote() {

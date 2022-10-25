29e3e19ff2ab86305c901dcff7561e3758b0e0fe
Ria Kapoor <S3973093@student.rmit.edu.au>
2022-10-25 13:30:45 +1300

    create jdbc connection to healthConditions

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -395,4 +395,53 @@ public class JDBCConnection {
         // Finally we return all of the movies
         return ST22Results;
     }
-}
+
+    //Generate a list of all the health conditions for the drop down
+    public ArrayList<String> getHealthConditions() {
+        ArrayList<String> healthCond = new ArrayList<String>();
+    
+        // Setup the variable for the JDBC connection
+        Connection connection = null;
+    
+        try {
+            // Connect to JDBC data base
+            connection = DriverManager.getConnection(DATABASE);
+    
+            // Prepare a new SQL Query & Set a timeout
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+    
+            // The Query
+            String query = "SELECT DISTINCT(condition) FROM LTHCStatistics";
+            System.out.println(query);
+            
+            // Get Result
+            ResultSet results = statement.executeQuery(query);
+    
+            // Process all of the results
+            while (results.next()) {
+                healthCond.add(results.getString("condiiton"));
+            }
+    
+            // Close the statement because we are done with it
+            statement.close();
+        } catch (SQLException e) {
+            // If there is an error, lets just pring the error
+            System.err.println(e.getMessage());
+        } finally {
+            // Safety code to cleanup
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+    
+        // Finally we return all of the movies
+        return healthCond;
+    }
+
+} // Keep as last bracket

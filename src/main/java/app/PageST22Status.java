d19a99fd13e90a6b62e1917c988c46fc4e0d683a
Marielle Lopez <s3922406@student.rmit.edu.au>
2022-10-31 19:40:09 +1100

    Cleaned ST22 Status jave file

--- src/main/java/app/PageST22Status.java
+++ src/main/java/app/PageST22Status.java
@@ -8,16 +8,6 @@ import java.util.Map;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
-/**
- * Example Index HTML class using Javalin
- * <p>
- * Generate a static HTML page using Javalin
- * by writing the raw HTML into a Java String object
- *
- * @author Timothy Wiley, 2021. email: timothy.wiley@rmit.edu.au
- * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
- */
-
 public class PageST22Status implements Handler {
 
     // URL of this page relative to http://localhost:7001/

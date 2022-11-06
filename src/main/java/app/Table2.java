114314e030a8b254cbfd27dde7aea0107a090fb3
Ria Kapoor <S3973093@student.rmit.edu.au>
2022-11-06 20:32:37 +1300

    updated page 5

---
+++ src/main/java/app/Table2.java
@@ -0,0 +1,131 @@
+
+    package app;
+
+    public class Table2 {
+    
+        public String code; 
+        public String name;
+        public String area;
+        public String propIndig_ISA;
+        public String propIndig_IS;
+        public String propIndig_IA;
+        public String propIndig_SA;
+        public String propIndig_I;
+        public String propIndig_S;
+        public String propIndig_A;
+        public String gap_inc21;
+        public String gap_inc16;
+        public String change_inc;
+        public String gap_sch21;
+        public String gap_sch16;
+        public String change_sch;
+        public String gap_age21;
+        public String gap_age16;
+        public String change_age;
+    
+        public Table2() {
+        }
+    
+        public Table2(String code, String name, String area, String propIndig_ISA, String propIndig_IS, String propIndig_IA, String propIndig_SA, String propIndig_I, String propIndig_S, String propIndig_A, 
+        String gap_inc21, String gap_inc16, String change_inc, String gap_sch21, String gap_sch16, String change_sch, String gap_age21, String gap_age16, String change_age) {
+            this.code = code; 
+            this.name = name; 
+            this.area = area;
+            this.propIndig_ISA = propIndig_ISA;
+            this.propIndig_IS = propIndig_IS; 
+            this.propIndig_IA = propIndig_IA;
+            this.propIndig_SA = propIndig_SA;
+            this.propIndig_I = propIndig_I;
+            this.propIndig_S = propIndig_S;
+            this.propIndig_A = propIndig_A;
+            this.gap_inc21 = gap_inc21; 
+            this.gap_inc16 = gap_inc16;
+            this.change_inc = change_inc;
+            this.gap_sch21 = gap_sch21;
+            this.gap_sch16 = gap_sch16;
+            this.change_sch = change_sch;
+            this.gap_age21 = gap_age21; 
+            this.gap_age16 = gap_age16;
+            this.change_age = change_age;
+        }
+    
+        public String getCode() {
+            return code;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+        public String getArea() {
+            return area;
+        }
+    
+        public String getPropIndig_ISA() {
+            return propIndig_ISA;
+        }
+    
+    
+        public String getPropIndig_IS() {
+            return propIndig_IS;
+        }
+    
+        public String getPropIndig_IA() {
+            return propIndig_IA;
+        }
+    
+        public String getPropIndig_SA() {
+            return propIndig_SA;
+        }
+    
+        public String getPropIndig_I() {
+            return propIndig_I;
+        }
+    
+        public String getPropIndig_S() {
+            return propIndig_S;
+        }
+    
+        public String getPropIndig_A() {
+            return propIndig_A;
+        }
+    
+        public String getGap_Inc21() {
+            return gap_inc21;
+        }
+    
+        public String getGap_Inc16() {
+            return gap_inc16;
+        }
+    
+        public String getChange_Inc() {
+            return change_inc;
+        }
+    
+        public String getGap_Sch21() {
+            return gap_sch21;
+        }
+        
+        public String getGap_Sch16() {
+            return gap_sch16;
+        }
+    
+        public String getChange_Sch() {
+            return change_sch;
+        }
+    
+        public String getGap_Age21() {
+            return gap_age21;
+        }
+    
+        public String getGap_Age16() {
+            return gap_age16;
+        }
+    
+        public String getChange_Age() {
+            return change_age;
+        }
+    
+    }
+    
+    

615f7f950a85a58e0e2d1ddf9a48635a806cad67
Marielle Lopez <s3922406@student.rmit.edu.au>
2022-11-06 12:50:29 +1100

    Implemented Thymeleaf

--- src/main/java/app/PageResources.java
+++ src/main/java/app/PageResources.java
@@ -1,6 +1,8 @@
 package app;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
@@ -19,93 +21,17 @@ public class PageResources implements Handler {
     // URL of this page relative to http://localhost:7001/
     public static final String URL = "/page7.html";
 
+    // Name of the Thymeleaf HTML template page in the resources folder
+    private static final String TEMPLATE = ("PageResources.html");
+
     @Override
     public void handle(Context context) throws Exception {
-        // Create a simple HTML webpage in a String
-        String html = "<html>";
-
-        // Add some Head information
-        html = html + "<head>" + 
-               "<title>Resources</title>";
-
-        // Add some CSS (external file)
-        html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
-        html = html + "</head>";
-
-        // Add the body
-        html = html + "<body>";
-
-        // Add the topnav
-        // This uses a Java v15+ Text Block
-        html = html + """
-            <div class='topnav'>
-                    <img class='topnav-logo' src='newlogo white.png' alt='logo' height='50'>
-                    <ul>
-                        <a href='/'>Home</a>
-                        <a href='mission.html'>Mission Statement</a>
-                        <div class ='dropdown'>
-                            <button class='dropbtn'>Data Analyses
-                                <i class='fa fa-caret-down'></i>
-                            </button>
-                            <div class='dropdown-content'>
-                                <a href='page3.html'>2021 Census Results</a>
-                                <a href='page4.html'>Changes between 2016 and 2021 Censuses</a>
-                                <a href='page5.html'>Gap Score between Indigenous and Non-Indigenous</a>
-                                <a href='page6.html'>Compare LGAs with Similar Characteristics</a>
-                            </div>
-                        </div>
-                        <a href='page7.html'>Resources</a>
-                    </ul>
-                    <input type='text' placeholder='Search...'>
-            </div>
-        """;
-
-        // Add header content block
-        html = html + """
-            <div class='header'>
-                <h1>Resources</h1>
-            </div>
-        """;
-
-        // Add Div for page Content
-        html = html + "<div class='content'>";
-
-        // Links to all the HTML pages and page informtion/ overview - MIGHT NOT NEED THIS BIT       
-        html = html + """
-            <h1>Websites we used for inspiration</h1>
-            <p><a href='https://healthinfonet.ecu.edu.au/'>HealthInfo</a> provides access to research in the health of Aboriginals and Torres Strait Islanders, 
-            as well as information on existing programs, policies, organisations, publications, and other resources on varying health topics.</p>
-            <p><a href='https://antar.org.au/'>ANTaR</a> is an advocacy organisation. The purpose of this website is to engage, educate and mobilise a broad community 
-            movement to advocate for justice, rights and respect for the First People of Australia.</p>
-            """;
-
-        // Close Content div
-        html = html + "</div>";
-
-        // Footer
-        html = html + """
-            <div class = 'footer'>
-                <h3>
-                    Acknowledgement of Country
-                </h3>
-                <p id = 'acknowledgementText'>
-                    Group 11 acknowledge and pay respect to Elders, both past and present, and all 
-                    generations of Aboriginal and Torres Strait <br> Islander People now and into the 
-                    future as the Traditional Owners of this land.
-                </p>
-                <p id = 'teamMemberCredit'>
-                    COSC2803 - Milestone 2 - This website was made by Ria Kapoor (s3973093) and Marielle Louisse Lopez (s3922406).
-                </p>
-            </div>
-        """;
-
-        // Finish the HTML webpage
-        html = html + "</body>" + "</html>";
-        
+        // The model of data to provide to Thymeleaf
+        Map<String, Object> model = new HashMap<String, Object>();
 
         // DO NOT MODIFY THIS
-        // Makes Javalin render the webpage
-        context.html(html);
+        // Makes Javalin render the webpage using Thymeleaf
+        context.render(TEMPLATE, model);
     }
 
 }
